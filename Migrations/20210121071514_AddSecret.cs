591cec56c37ddb0428b6144ebc52271fd69ce227
gitstrato <gitstrato@4by.eu>
2021-01-25 08:09:53 +0100

    Initial From existing offline file system

---
+++ Migrations/20210121071514_AddSecret.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApiTodoList.Migrations
+{
+    public partial class AddSecret : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Secret",
+                table: "TodoItems",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Secret",
+                table: "TodoItems");
+        }
+    }
+}
